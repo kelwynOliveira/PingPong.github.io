6824be7ecc5cded4ebc478ef6d0f8780afe0d16f
Kelwyn Oliveira <kelwyn.oliveira@gmail.com>
2023-06-24 12:19:49 -0400

    pingpong game desktop

--- src/games/pingpong.js
+++ src/games/pingpong.js
@@ -1,8 +1,7 @@
 const canvasPlace = document.querySelector('#gameDiv').querySelector('canvas');
 const ctx = canvasPlace.getContext("2d");
-
-//General
-let colorFill = "#fff";
+const scoreLeftPlayer = document.querySelector("#leftPlayer");
+const scoreRightPlayer = document.querySelector("#rightPlayer");
 
 //canvas dimmension
 let tableWidth = canvasPlace.width;
@@ -12,8 +11,12 @@ let tableHight = canvasPlace.height;
 let xBall = tableWidth/2;
 let yBall = tableHight/2;
 let radiusBall = 4;
-let xVelocity = 2;
+let xVelocity = 3;
 let yVelocity = 2;
+let ballLeftside = xBall - radiusBall;
+let ballRightSide = xBall + radiusBall;
+let ballTopSide = yBall - radiusBall;
+let ballBottom = yBall + radiusBall;
 
 //Racket
 let racketWidth = 2;
@@ -21,26 +24,44 @@ let racketHight = 30;
 let yRacketLeft = (tableHight-racketHight)/2;
 let xRacketLeft = 5;
 let yRacketRight = (tableHight-racketHight)/2;
-let xRacketRight = tableWidth - racketWidth - 5;
+let xRacketRight = tableWidth - xRacketLeft - racketWidth;
 
+//Racket moviments
+let racketLeftUp = 'KeyW';
+let racketLeftDown = 'KeyS';
+let racketRightUp = 'ArrowUp';
+let racketRightDown = 'ArrowDown';
 
+//General
+let colorFill = "#fff";
+let colisionPoint = racketWidth+xRacketLeft;
+let scoreLeft = 0;
+let scoreRight = 0;
 
+function firstScreen(){
+    xBall = tableWidth/2;
+    yBall = tableHight/2;
+    yRacketLeft = yRacketRight = (tableHight-racketHight)/2;
+    draw();
+}
+firstScreen();
 
-circle(xBall, yBall, radiusBall, colorFill);
-rect(xRacketLeft, yRacketLeft, racketWidth, racketHight, colorFill);
-rect(xRacketRight, yRacketRight, racketWidth, racketHight, colorFill);
-
+//Start Game
 const btnStart = document.querySelector("#startBTN").querySelector("button");
 btnStart.addEventListener("click", startGame);
-
+let startDraw = null;
 function startGame(){
-    btnStart.textContent === "Start the game" ? btnStart.textContent = "Stop the game":btnStart.textContent = "Start the game";
-    setInterval(draw,20);
-    // let draw = setInterval(function(){
-    //     draw();
-    //     if (start === false){clearInterval(draw)}
-    // },20)
-
+    if (btnStart.textContent === "Start the game"){
+        btnStart.textContent = "Stop the game";
+        startDraw = setInterval(draw,30);
+    }else{
+        btnStart.textContent = "Start the game";
+        clearInterval(startDraw);
+        startDraw = null;
+        firstScreen();
+        scoreLeft = scoreRight = 0;
+        score(scoreLeft, scoreRight);
+    }
 }
 
 function draw(){
@@ -48,12 +69,11 @@ function draw(){
     circle(xBall, yBall, radiusBall, colorFill);
     rect(xRacketLeft, yRacketLeft, racketWidth, racketHight, colorFill);
     rect(xRacketRight, yRacketRight, racketWidth, racketHight, colorFill);
-    // racketLeftMovement(racketLeftUp, racketLeftDown);
-    // racketRightMovement(racketRightUp, racketRightDown);
+    racketLeftMovement(racketLeftUp, racketLeftDown);
+    racketRightMovement(racketRightUp, racketRightDown);
     racketColision();
     ballMoviment();
 }
-
 
 function clearDraw(){
     ctx.clearRect(0, 0, tableWidth, tableHight);
@@ -75,28 +95,75 @@ function rect(x, y, width, height, color){
 function ballMoviment(){
     xBall += xVelocity;
     yBall += yVelocity;
-    if (xBall+radiusBall > tableWidth || xBall-radiusBall < 0){
+    ballLeftside = xBall - radiusBall;
+    ballRightSide = xBall + radiusBall;
+
+    if (xBall >= tableWidth){
+        scoreLeft++;
+        xVelocity *= -1;
+    }
+    if (xBall <= 0){
+        scoreRight++;
         xVelocity *= -1;
     }
     if (yBall+radiusBall > tableHight || yBall-radiusBall < 0){
         yVelocity *= -1;
     }
+    score(scoreLeft, scoreRight);
 }
 
 function racketColision() {
-    let ballLeftside = xBall - radiusBall;
-    let ballRightSide = xBall + radiusBall;
-    let ballTopSide = yBall - radiusBall;
-    let ballBottom = yBall + radiusBall;
+    ballLeftside = xBall - radiusBall;
+    ballRightSide = xBall + radiusBall;
+    ballTopSide = yBall - radiusBall;
+    ballBottom = yBall + radiusBall;
 
-    if (ballLeftside < xRacketLeft + racketWidth
+    if (ballLeftside <= xRacketLeft + racketWidth
         && ballTopSide < yRacketLeft + racketHight
         && ballBottom > yRacketLeft) {
+        xBall = colisionPoint+radiusBall;
         xVelocity *= -1;
     }
-    if (ballRightSide > xRacketRight
+    if (ballRightSide+1 >= xRacketRight
         && ballTopSide < yRacketRight + racketHight
         && ballBottom > yRacketRight) {
+        xBall = tableWidth-colisionPoint-radiusBall;
         xVelocity *= -1;
     }
+}
+
+//Racket movements
+//Prevent scrow with arrows
+window.addEventListener("keydown", function(e) {
+    if(["ArrowUp","ArrowDown"].indexOf(e.code) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
+function racketRightMovement(keyUp, keyDown){
+    document.addEventListener('keydown', (event) => {
+        if(event.code == keyUp && yRacketRight > 0) {
+            yRacketRight -= 1/100;
+        }
+        if(event.code == keyDown && yRacketRight < (tableHight-racketHight)) {
+            yRacketRight += 1/100;
+        }
+      }, false);
+}
+
+function racketLeftMovement(keyUp, keyDown){
+    document.addEventListener('keydown', (event) => {
+        if(event.code == keyUp && yRacketLeft > 0) {
+            yRacketLeft -= 1/100;
+        }
+        if(event.code == keyDown && yRacketLeft < (tableHight-racketHight)) {
+            yRacketLeft += 1/100;
+        }
+      }, false);
+}
+
+//Score
+function score(left, right){
+    scoreLeftPlayer.textContent = left;
+    scoreRightPlayer.textContent = right;
 }
