2c70cf96d8259a026c97353b69c4ba5ad1f8ec63
Kelwyn Oliveira <kelwyn.oliveira@gmail.com>
2023-06-24 19:32:39 -0400

    Creation of the snake game

---
+++ src/games/snake.js
@@ -0,0 +1,28 @@
+const canvasPlace = document.querySelector('#gameDiv').querySelector('canvas');
+const ctx = canvasPlace.getContext("2d");
+const controls = document.querySelector("#controls");
+
+
+
+const btnStart = document.querySelector("#startBTN").querySelector("button");
+btnStart.addEventListener("click", startGame);
+let startDraw = null;
+function startGame(){
+    if (btnStart.textContent === "Start the game"){
+        btnStart.textContent = "Stop the game";
+        if(isTouchDevice)controls.style.display = "flex";
+    }else{
+        btnStart.textContent = "Start the game";
+        clearInterval(startDraw);
+        startDraw = null;
+        if (isTouchDevice)controls.style.display = "none";
+    }
+}
+
+
+//Touch devices
+function isTouchDevice() {
+    return (('ontouchstart' in window) ||
+    (navigator.maxTouchPoints > 0) ||
+    (navigator.msMaxTouchPoints > 0));
+}
