0e341e1d64e708b408d6282a86e07144ecc256f8
Kelwyn Oliveira <kelwyn.oliveira@gmail.com>
2023-07-02 06:08:53 -0400

    Disable scroll on mobile menu

--- functions.js
+++ functions.js
@@ -1,10 +1,18 @@
-socialDiv = document.querySelector('#social-div-mobile');
+socialDiv = document.querySelector("#social-div-mobile");
 
-function showHide(el){
-    display = document.querySelector(el);
-    display.style.display === 'block'? display.style.display = 'none' : display.style.display = 'block';
+function showHide(el) {
+  display = document.querySelector(el);
+  display.style.display === "block"
+    ? (display.style.display = "none")
+    : (display.style.display = "block");
 }
 
-function mobileMenu(){
-    socialDiv.style.display === 'flex'? socialDiv.style.display = 'none' : socialDiv.style.display = 'flex';
+function mobileMenu() {
+  if (socialDiv.style.display === "flex") {
+    socialDiv.style.display = "none";
+    document.body.style.overflow = "visible";
+  } else {
+    socialDiv.style.display = "flex";
+    document.body.style.overflow = "hidden";
+  }
 }
